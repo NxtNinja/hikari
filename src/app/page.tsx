a2ccbef0208c34bddffe875ab89c50cfc2c415b1
NxtNinja <136341993+NxtNinja@users.noreply.github.com>
2025-02-08 15:51:16 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import getAllAnime from "@/hooks/anime/getAllAnime";
-
-const Home = async () => {
-  const { data, isError, error } = await getAllAnime();
-
-  console.log(data?.data, isError, error);
-
+const Home = () => {
   return (
     <>
-      <div className="w-full">{/* <DisplayAnimeCards /> */}</div>
+      <div className=""></div>
     </>
   );
 };
